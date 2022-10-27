7289743b2cd5613c7492da8358b8d79d0497045b
Mac S <mac.src@gmail.com>
2022-10-27 19:40:05 +0530

    data removed

--- python/ks_api_client/settings.py
+++ python/ks_api_client/settings.py
@@ -1,10 +1,10 @@
-access_token = "7abce22b-263e-36d0-b0c3-fbe6304bc517"
-userid = "SH3654"
-consumer_key = "KZifCybgPTZF8fTRfkuNNKK0WeQa"
-consumer_secret = "eTIYRPzU0Q5dL_gL_2bWBaIifWYa"
-broadcast_host = "https://wstreamer.kotaksecurities.com/feed"
-ip = "127.0.0.1"
-app_id = "DefaultApplication"
+access_token = "ssss"
+userid = "ssss"
+consumer_key = "ssss"
+consumer_secret = "ssss"
+broadcast_host = "xxxx"
+ip = "xxxxx"
+app_id = "xxxxxx"
 proxy_url = ""
 proxy_user = ""
 proxy_pass = ""
@@ -12,8 +12,8 @@ cacert_file = ""
 cacert_dir = ""
 
 #  ##### added information
-password = "M_K@10E22g"
+password = ""
 
 # session - 26102022
 
-session = "CD0AD89D1288C30E5C18BC32633C4B570297E7"
+session = "xxxx"
